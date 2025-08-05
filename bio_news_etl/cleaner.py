78833b89dbdc2d3ab2b477052966b2c38db1b8f7
IH <147421162+ifhDev@users.noreply.github.com>
2025-08-05 22:26:30 +0200

    filled out cleaner

--- bio_news_etl/cleaner.py
+++ bio_news_etl/cleaner.py
@@ -1,3 +1,25 @@
 def clean_article_data(article):
     # standardises/validates fields
-    pass
+    required_fields = {
+        "type": None,
+        "date": None,
+        "title": "",
+        "doi": None,
+        "authors": [],
+    }
+    for key, default in required_fields.items():
+        if key not in article or article[key] is None or article[key] == "":
+            article[key] = default
+    authors = article.get("authors")
+    if isinstance(authors, str):
+        # in case author splitting didn't work on entry
+        if ";" in authors:
+            # for publications that use the "Surname, Name" splitting
+            author_list = [a.strip() for a in authors.split(";") if a.strip()]
+        else:
+            # for everything else
+            author_list = [a.strip() for a in authors.split(",") if a.strip()]
+        article["authors"] = author_list
+    elif authors is None:
+        article["authors"] = []
+    return article
